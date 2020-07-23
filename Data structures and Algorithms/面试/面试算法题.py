3955781ec532b4bdd7912c6c3b07566299b02b17
yangwen_i <yangwen_i@didi_global.com>
2020-07-23 10:50:27 +0800

    add files

--- Data structures and Algorithms/面试/面试算法题.py
+++ Data structures and Algorithms/面试/面试算法题.py
@@ -13,30 +13,6 @@ def count_str(input_str):
 
 
 """
-合并有序数组
-
-循环比较两个有序数组头位元素的大小，并把较小的元素放到新数组中，从老数组中删掉,
-直到其中一个数组长度为0。然后再把不为空的老数组中剩下的部分加到新数组的结尾。
-"""
-
-
-def merge_sort(a, b):
-    result = []
-    while len(a) > 0 and len(b) > 0:
-        if a[0] <= b[0]:
-            result.append(a[0])
-            a.remove(a[0])
-        if b[0] <= a[0]:
-            result.append(b[0])
-            b.remove(b[0])
-    if len(a) == 0:
-        result += b
-    if len(b) == 0:
-        result += a
-    return result
-
-
-"""
 反转字符串
 """
 
@@ -91,6 +67,30 @@ def lengthOfLongestSubstring(input_str):
             max_len = cur_len
 
     return max_len
+
+
+"""
+合并有序数组
+
+循环比较两个有序数组头位元素的大小，并把较小的元素放到新数组中，从老数组中删掉,
+直到其中一个数组长度为0。然后再把不为空的老数组中剩下的部分加到新数组的结尾。
+"""
+
+
+def merge_sort(a, b):
+    result = []
+    while len(a) > 0 and len(b) > 0:
+        if a[0] <= b[0]:
+            result.append(a[0])
+            a.remove(a[0])
+        if b[0] <= a[0]:
+            result.append(b[0])
+            b.remove(b[0])
+    if len(a) == 0:
+        result += b
+    if len(b) == 0:
+        result += a
+    return result
 
 
 if __name__ == '__main__':

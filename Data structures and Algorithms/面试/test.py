ef52814caed42ac8ef4b9710166bebf6206a196a
yangwen_i <yangwen_i@didi_global.com>
2020-09-10 22:54:52 +0800

    update

--- Data structures and Algorithms/面试/test.py
+++ Data structures and Algorithms/面试/test.py
@@ -1,17 +1,34 @@
-import re
+'''
+读取大型日志文件，搜索关键字，打印关键字前5行
+'''
+from collections import deque
 
-# with open('/Users/didi/Desktop/My_Study/Data structures and Algorithms/log.txt', 'r') as f:
-#     alllines = f.readlines()
-#
-# with open('/Users/didi/Desktop/My_Study/Data structures and Algorithms/log.txt', 'w+') as f:
-#     for eachline in alllines:
-#         a = re.sub('response', 'hi', eachline)
-#         f.writelines(a)
 
-with open('/Users/didi/Desktop/My_Study/Data structures and Algorithms/log.txt', 'r+') as f1:
-    with open('/Users/didi/Desktop/My_Study/Data structures and Algorithms/log2.txt', 'w+') as f2:
-        str1 = 'response'
-        str2 = 'hi'
-        for ss in f1.readlines():
-            tt = re.sub(str1, str2, ss)
-            f2.write(tt)
+# 定义一个搜索关键字的函数
+def search_keyword(lines, pattern, history=5):
+    """
+    搜索关键字的生成器函数
+    :param lines: 含有多行内容的可迭代对象
+    :param pattern: 要搜索的关键字
+    :param history: 保留的含有关键字的条目数
+    """
+    # 创建只能容纳 5 个元素的队列
+    previous_lines = deque(maxlen=history)
+
+    for line in lines:
+
+        # 判断每行是否含有目标关键字
+        if pattern in line:
+            # 当每次匹配成后就把此行添加到队列中，
+            # 并且返回这个队列，以便稍后打印里面的
+            # 元素时可以看到其中数量的变化
+            previous_lines.append(line)
+            yield previous_lines
+
+
+if __name__ == '__main__':
+    with open(r'/Users/didi/Desktop/My_Study/Data structures and Algorithms/log.txt') as f:
+        for prevlines in search_keyword(f, 'response', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print('\n', '-' * 20)

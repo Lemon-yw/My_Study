e082e4c4c93b09ae8479acb25af2b0bd7812d4b6
Lemon-yw <474768654@qq.com>
2020-10-28 00:01:33 -0700

    update

--- Data structures and Algorithms/test2.py
+++ Data structures and Algorithms/test2.py
@@ -1,11 +1,1 @@
-def bubble_sort(array):
-    length = len(array)
-    for i in range(length - 1):
-        for j in range(length - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-    return array
-
-
-nums = [3, 2, 5, 4]
-print(bubble_sort(nums))
+exec("print('haha')")
